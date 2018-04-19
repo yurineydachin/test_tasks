e301a784e1425a434a7ba0fc78187552d4a6fc85
Yuri Neudachin <yurineydachin@mail.ru>
2018-04-19 20:20:57 +0300

    add more tests for bakery

--- ruby_backery/tc_bakery.rb
+++ ruby_backery/tc_bakery.rb
@@ -6,8 +6,8 @@ class TestBakery < Test::Unit::TestCase
     def test_findPacksAndCount
         assert_equal({1 => 10}, findPacksAndCount(10, [1]))
         assert_equal({2 => 5}, findPacksAndCount(10, [2]))
-        assert_equal(nil, findPacksAndCount(10, [3]))
-        assert_equal(nil, findPacksAndCount(10, [4]))
+        assert_nil(findPacksAndCount(10, [3]))
+        assert_nil(findPacksAndCount(10, [4]))
         assert_equal({5 => 2}, findPacksAndCount(10, [5]))
 
         assert_equal({3 => 3, 1 => 1}, findPacksAndCount(10, [3,1]))
@@ -63,6 +63,17 @@ class TestBakery < Test::Unit::TestCase
         assert_equal("10 VS5 $17.98\n    2 x 5 $8.99\n", orderItem.to_s)
     end
 
+    def test_createOrderItem_wrongCount
+        p1 = Pack.new(3, 6.99)
+        p2 = Pack.new(5, 8.99)
+        orderItem = OrderItem.new(Product.new("VS5", "Vegemite Scroll", [p1, p2]), 7)
+
+        assert_equal(7, orderItem.count)
+        assert_equal({}, orderItem.packHash)
+        assert_equal(0, orderItem.totalPrice)
+        assert_equal("7 VS5 $0.00\n", orderItem.to_s)
+    end
+
     def test_createOrder
         p1 = Pack.new(3, 6.99)
         p2 = Pack.new(5, 8.99)
@@ -76,4 +87,37 @@ class TestBakery < Test::Unit::TestCase
         assert_equal("10 VS5 $17.98\n    2 x 5 $8.99\n", order.to_s)
     end
 
+    def test_createOrder_2Products
+        p1 = Pack.new(3, 6.99)
+        p2 = Pack.new(5, 8.99)
+        order = Order.new
+        order.addItem(Product.new("VS5", "Vegemite Scroll", [p1, p2]), 10)
+        order.addItem(Product.new("MB11", "Blueberry Muffin", [p1, p2]), 14)
+
+        assert_equal(2, order.items.length)
+
+        assert_equal(10, order.items[0].count)
+        assert_equal({p2 => 2}, order.items[0].packHash)
+        assert_equal(17.98, order.items[0].totalPrice)
+        assert_equal("10 VS5 $17.98\n    2 x 5 $8.99\n", order.items[0].to_s)
+
+        assert_equal(14, order.items[1].count)
+        assert_equal({p2 => 1, p1 => 3}, order.items[1].packHash)
+        assert_equal(29.96, order.items[1].totalPrice)
+        assert_equal("14 MB11 $29.96\n    1 x 5 $8.99\n    3 x 3 $6.99\n", order.items[1].to_s)
+
+        assert_equal("10 VS5 $17.98\n    2 x 5 $8.99\n14 MB11 $29.96\n    1 x 5 $8.99\n    3 x 3 $6.99\n", order.to_s)
+    end
+
+    def test_createBakery
+        p1 = Pack.new(3, 6.99)
+        p2 = Pack.new(5, 8.99)
+        order = BakeryComposer.new([Product.new("VS5", "Vegemite Scroll", [p1, p2])]).calculateOrder({"VS5" => 10})
+
+        assert_equal(1, order.items.length)
+        assert_equal(10, order.items[0].count)
+        assert_equal({p2 => 2}, order.items[0].packHash)
+        assert_equal(17.98, order.items[0].totalPrice)
+        assert_equal("10 VS5 $17.98\n    2 x 5 $8.99\n", order.to_s)
+    end
 end
